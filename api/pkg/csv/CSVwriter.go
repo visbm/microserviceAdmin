659a952252f7e49dfcb5f1d41d29915f6ce30d64
visbm <visbm@mail.ru>
2022-02-02 12:10:37 +0300

    create download

--- api/pkg/csv/CSVwriter.go
+++ api/pkg/csv/CSVwriter.go
@@ -10,11 +10,11 @@ import (
 )
 
 //MakeCSV make csv file and write data
-func MakeCSV(data interface{}, name string) (string,error) {
+func MakeCSV(data interface{}, name string) (string, error) {
 
 	gocsv.SetCSVWriter(func(out io.Writer) *gocsv.SafeCSVWriter {
 		writer := csv.NewWriter(out)
-		writer.Comma = '\t'
+		writer.Comma = '-'
 		return gocsv.NewSafeCSVWriter(writer)
 	})
 
@@ -23,13 +23,13 @@ func MakeCSV(data interface{}, name string) (string,error) {
 	file, err := os.Create(path)
 
 	if err != nil {
-		return "" , err
+		return "", err
 	}
 	defer file.Close()
 
 	err = gocsv.MarshalFile(data, file)
 	if err != nil {
-		return "" ,err
+		return "", err
 	}
-	return path ,nil
+	return path, nil
 }
--- api/webapp/handlersAdmin/usersHandlers/printAllUsersCSV.go
+++ api/webapp/handlersAdmin/usersHandlers/printAllUsersCSV.go
@@ -35,6 +35,7 @@ func PrintAllUsersCSV(s *store.Store) httprouter.Handle {
 			return
 		}
 		name := "allusers"
+		
 		path, err := csv.MakeCSV(users, name)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusNotFound)
@@ -42,15 +43,15 @@ func PrintAllUsersCSV(s *store.Store) httprouter.Handle {
 			return
 		}
 
-		s.Logger.Info("Csv is created")
-		s.Logger.Info("path:  ", path)
+		s.Logger.Info("Csv is created")		
 
-		url := fmt.Sprint("/admin/users/download/", name, ".csv")
+		url := fmt.Sprint("http://localhost:8080/admin/users/download/", name, ".csv")
 		s.Logger.Info("url: ", url)
+		
 
-		http.Redirect(w, r, "/admin/homeusers", http.StatusFound)
-		//http.Redirect(w, r.WithContext(context.WithValue(r.Context(), middlewear.CtxKeyFile, path)), url, http.StatusFound)
-		//next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(),middlewear.CtxKeyFile, path)))
+		http.ServeFile(w, r, path)
+
+		//http.Redirect(w, r, "/admin/homeusers", http.StatusFound)	
 
 	}
 }
